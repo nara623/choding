c899ae620aaebfacf97d304a192623c1bbb3c56f
nara623 <nara623@gmail.com>
2015-02-15 16:18:54 +0900

    Revert "Revert "TimeComplexity""
    This reverts commit 96dd67c5475dad260287e330f0822b16f0c1cf72.

---
+++ src/choding/TimeComplexity.scala
@@ -0,0 +1,18 @@
+package choding
+
+/**
+ * Created by Eunsook on 2015-02-15.
+ */
+object TimeComplexity {
+  def sum(a:Int, b:Int):Int={
+  println(a)
+    a+b
+  }
+  def main(args: Array[String]) {
+    val a = readLine.toInt
+    val b = readLine.toInt
+    println(b)
+    println(sum(b,a))
+  }
+
+}
